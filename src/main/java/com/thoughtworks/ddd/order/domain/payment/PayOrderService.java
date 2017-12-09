cfed1b8df5256baefb9adcb37852ca3ea3a3252c
Yan Qian <qianyan@thoughtworks.com>
2017-12-09 09:04:51 +0800

    rename packages

--- src/main/java/com/thoughtworks/ddd/order/domain/payment/PayOrderService.java
+++ src/main/java/com/thoughtworks/ddd/order/domain/payment/PayOrderService.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PayOrderService {
+public class  PayOrderService {
     private PaymentRepository paymentRepository;
 
     @Autowired
